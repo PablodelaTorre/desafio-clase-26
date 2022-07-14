3ca6baca01470ea9442a12b9fdd1821e1b5dab9b
Pablo <delatorre.pablodaniel@gmail.com>
2022-07-14 17:36:36 -0300

    probando passport

--- servidor.js
+++ servidor.js
@@ -6,6 +6,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 //import'./src/dataBases/database.js'
 //import passport from 'passport';
+import './src/passport/local.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // import { ioServer } from 'socket.io'
